564ae2791375f6a532673f62aafa94c2d5ded5a1
Joel Junior <joeljunior05@hotmail.com>
2017-07-12 10:59:58 -0300

    Changing FFT implementation, now it is equal to OpenCV 2.4.*

--- lib/granul.cpp
+++ lib/granul.cpp
@@ -590,132 +590,191 @@ static const cv::Scalar ZERO(0);
 
 static const cv::Scalar WHITE(255, 255, 255);
 
-// Fourier transform performance is not a monotonic function of a vector
-// size - matrices whose dimensions are powers of two are the fastest to
-// process, and multiples of 2, 3 and 5 (for example, 300 = 5*5*3*2*2) are
-// also processed quite efficiently. Therefore it makes sense to pad input
-// data with zeros to get a bit larger matrix that can be transformed much
-// faster than the original one.
-cv::Size fit(const cv::Size &size)
+//CrossCorrelation FFT
+
+void crossCorr( const Mat& img, const Mat& _templ, Mat& corr,
+                Size corrsize, int ctype,
+                Point anchor, double delta, int borderType )
 {
-  return cv::Size(cv::getOptimalDFTSize(size.width),
-                  cv::getOptimalDFTSize(size.height));
-}
+    const double blockScale = 4.5;
+    const int minBlockSize = 256;
+    std::vector<uchar> buf;
 
-cv::Mat F_fwd(const cv::Mat &I, const cv::Size &size)
-{
-  // Pad input matrix to given size.
-  cv::Mat P;
-  int m = size.height - I.rows;
-  int n = size.width - I.cols;
-  cv::copyMakeBorder(I, P, 0, m, 0, n, cv::BORDER_CONSTANT, ZERO);
+    Mat templ = _templ;
+    int depth = img.depth(), cn = img.channels();
+    int tdepth = templ.depth(), tcn = templ.channels();
+    int cdepth = CV_MAT_DEPTH(ctype), ccn = CV_MAT_CN(ctype);
 
-  // Compute Fourier transform for input data. The last argument
-  // informs the dft() function of how many non-zero rows are there,
-  // so it can handle the rest of the rows more efficiently and save
-  // some time.
-  cv::Mat F;
-  cv::dft(P, F, 0, I.rows);
+    CV_Assert( img.dims <= 2 && templ.dims <= 2 && corr.dims <= 2 );
 
-  return F;
-}
-
-cv::Mat F_inv(const cv::Mat &F, const cv::Size &size)
-{
-  // Compute inverse Fourier transform for input data. The last
-  // argument informs the dft() function of how many non-zero
-  // rows are expected in the output, so it can handle the rest
-  // of the rows more efficiently and save some time.
-  cv::Mat I;
-  cv::dft(F, I, cv::DFT_INVERSE + cv::DFT_SCALE, size.height);
-
-  return I(cv::Rect(0, 0, size.width, size.height));
-}
-
-Mat fF_I;
-
-cv::Mat C(const cv::Mat &T, const cv::Mat &I, const cv::Size &size)
-{
-  // Compute the Fourier transforms of template and image.
-  cv::Mat F_T = F_fwd(T, size);
-  cv::Mat F_I = F_fwd(I, size);
-
-  // Compute the cross correlation in the frequency domain.
-  cv::Mat F_TI;
-  cv::mulSpectrums(F_I, F_T, F_TI, 0, true);
-
-  // Compute the inverse Fourier transform of the cross-correlation,
-  // dismissing those rows and columns of the cross-correlation
-  // matrix that would require the template to "roll over" the image.
-  cv::Size clipped;
-  clipped.width = I.cols - T.cols;
-  clipped.height = I.rows - T.rows;
-  return F_inv(F_TI, clipped);
-}
-
-cv::Mat FC(const cv::Mat &I, const cv::Mat &T, const cv::Size &size)
-{
-  // Compute the Fourier transforms of template and image.
-  cv::Mat F_T = F_fwd(T, size);
-
-  // Compute the cross correlation in the frequency domain.
-  cv::Mat F_TI;
-  cv::mulSpectrums(fF_I, F_T, F_TI, 0, true);
-
-  // Compute the inverse Fourier transform of the cross-correlation,
-  // dismissing those rows and columns of the cross-correlation
-  // matrix that would require the template to "roll over" the image.
-  cv::Size clipped;
-  clipped.width = I.cols - T.cols;
-  clipped.height = I.rows - T.rows;
-  return F_inv(F_TI, clipped);
-}
-
-
-cv::Mat W(const cv::Mat &T)
-{
-  return cv::Mat(T.size(), CV_64F, ONE);
-}
-
-cv::Point3f matchTemplate(const cv::Mat &T, const cv::Mat &I, cv::Mat &out)
-{
-  // Compute the optimal size for DFT computing.
-  cv::Size size = fit(I.size());
-
-  //Compute the cross-correlation and normalizing matrix.
-  cv::Mat C_TI = C(T, I, size);
-  cv::Mat M_I = C(W(T), I.mul(I), size);
-
-  int i_s, j_s;
-  float r = 0;
-  int rows = C_TI.rows;
-  int cols = C_TI.cols;
-  /*for (int i = 0; i < rows; i++)
-  {
-    for (int j = 0; j < cols; j++)
+    if( depth != tdepth && tdepth != std::max(CV_32F, depth) )
     {
-      float v = C_TI.at<FLT>(i, j) / sqrt(M_I.at<FLT>(i, j));
-      if (r < v)
-      {
-        r = v;
-        i_s = i;
-        j_s = j;
-      }
+        _templ.convertTo(templ, std::max(CV_32F, depth));
+        tdepth = templ.depth();
     }
-  }*/
 
-  out = C_TI.clone();
+    CV_Assert( depth == tdepth || tdepth == CV_32F);
+    CV_Assert( corrsize.height <= img.rows + templ.rows - 1 &&
+               corrsize.width <= img.cols + templ.cols - 1 );
 
-  return cv::Point3f(j_s, i_s, r);
+    CV_Assert( ccn == 1 || delta == 0 );
+
+    corr.create(corrsize, ctype);
+
+    int maxDepth = depth > CV_8S ? CV_64F : std::max(std::max(CV_32F, tdepth), cdepth);
+    Size blocksize, dftsize;
+
+    blocksize.width = cvRound(templ.cols*blockScale);
+    blocksize.width = std::max( blocksize.width, minBlockSize - templ.cols + 1 );
+    blocksize.width = std::min( blocksize.width, corr.cols );
+    blocksize.height = cvRound(templ.rows*blockScale);
+    blocksize.height = std::max( blocksize.height, minBlockSize - templ.rows + 1 );
+    blocksize.height = std::min( blocksize.height, corr.rows );
+
+    dftsize.width = std::max(getOptimalDFTSize(blocksize.width + templ.cols - 1), 2);
+    dftsize.height = getOptimalDFTSize(blocksize.height + templ.rows - 1);
+    if( dftsize.width <= 0 || dftsize.height <= 0 )
+        CV_Error( CV_StsOutOfRange, "the input arrays are too big" );
+
+    // recompute block size
+    blocksize.width = dftsize.width - templ.cols + 1;
+    blocksize.width = MIN( blocksize.width, corr.cols );
+    blocksize.height = dftsize.height - templ.rows + 1;
+    blocksize.height = MIN( blocksize.height, corr.rows );
+
+    Mat dftTempl( dftsize.height*tcn, dftsize.width, maxDepth );
+    Mat dftImg( dftsize, maxDepth );
+
+    int i, k, bufSize = 0;
+    if( tcn > 1 && tdepth != maxDepth )
+        bufSize = templ.cols*templ.rows*CV_ELEM_SIZE(tdepth);
+
+    if( cn > 1 && depth != maxDepth )
+        bufSize = std::max( bufSize, (blocksize.width + templ.cols - 1)*
+            (blocksize.height + templ.rows - 1)*CV_ELEM_SIZE(depth));
+
+    if( (ccn > 1 || cn > 1) && cdepth != maxDepth )
+        bufSize = std::max( bufSize, blocksize.width*blocksize.height*CV_ELEM_SIZE(cdepth));
+
+    buf.resize(bufSize);
+
+    // compute DFT of each template plane
+    for( k = 0; k < tcn; k++ )
+    {
+        int yofs = k*dftsize.height;
+        Mat src = templ;
+        Mat dst(dftTempl, Rect(0, yofs, dftsize.width, dftsize.height));
+        Mat dst1(dftTempl, Rect(0, yofs, templ.cols, templ.rows));
+
+        if( tcn > 1 )
+        {
+            src = tdepth == maxDepth ? dst1 : Mat(templ.size(), tdepth, &buf[0]);
+            int pairs[] = {k, 0};
+            mixChannels(&templ, 1, &src, 1, pairs, 1);
+        }
+
+        if( dst1.data != src.data )
+            src.convertTo(dst1, dst1.depth());
+
+        if( dst.cols > templ.cols )
+        {
+            Mat part(dst, Range(0, templ.rows), Range(templ.cols, dst.cols));
+            part = Scalar::all(0);
+        }
+        dft(dst, dst, 0, templ.rows);
+    }
+
+    int tileCountX = (corr.cols + blocksize.width - 1)/blocksize.width;
+    int tileCountY = (corr.rows + blocksize.height - 1)/blocksize.height;
+    int tileCount = tileCountX * tileCountY;
+
+    Size wholeSize = img.size();
+    Point roiofs(0,0);
+    Mat img0 = img;
+
+    if( !(borderType & BORDER_ISOLATED) )
+    {
+        img.locateROI(wholeSize, roiofs);
+        img0.adjustROI(roiofs.y, wholeSize.height-img.rows-roiofs.y,
+                       roiofs.x, wholeSize.width-img.cols-roiofs.x);
+    }
+    borderType |= BORDER_ISOLATED;
+
+    // calculate correlation by blocks
+    for( i = 0; i < tileCount; i++ )
+    {
+        int x = (i%tileCountX)*blocksize.width;
+        int y = (i/tileCountX)*blocksize.height;
+
+        Size bsz(std::min(blocksize.width, corr.cols - x),
+                 std::min(blocksize.height, corr.rows - y));
+        Size dsz(bsz.width + templ.cols - 1, bsz.height + templ.rows - 1);
+        int x0 = x - anchor.x + roiofs.x, y0 = y - anchor.y + roiofs.y;
+        int x1 = std::max(0, x0), y1 = std::max(0, y0);
+        int x2 = std::min(img0.cols, x0 + dsz.width);
+        int y2 = std::min(img0.rows, y0 + dsz.height);
+        Mat src0(img0, Range(y1, y2), Range(x1, x2));
+        Mat dst(dftImg, Rect(0, 0, dsz.width, dsz.height));
+        Mat dst1(dftImg, Rect(x1-x0, y1-y0, x2-x1, y2-y1));
+        Mat cdst(corr, Rect(x, y, bsz.width, bsz.height));
+
+        for( k = 0; k < cn; k++ )
+        {
+            Mat src = src0;
+            dftImg = Scalar::all(0);
+
+            if( cn > 1 )
+            {
+                src = depth == maxDepth ? dst1 : Mat(y2-y1, x2-x1, depth, &buf[0]);
+                int pairs[] = {k, 0};
+                mixChannels(&src0, 1, &src, 1, pairs, 1);
+            }
+
+            if( dst1.data != src.data )
+                src.convertTo(dst1, dst1.depth());
+
+            if( x2 - x1 < dsz.width || y2 - y1 < dsz.height )
+                copyMakeBorder(dst1, dst, y1-y0, dst.rows-dst1.rows-(y1-y0),
+                               x1-x0, dst.cols-dst1.cols-(x1-x0), borderType);
+
+            dft( dftImg, dftImg, 0, dsz.height );
+            Mat dftTempl1(dftTempl, Rect(0, tcn > 1 ? k*dftsize.height : 0,
+                                         dftsize.width, dftsize.height));
+            mulSpectrums(dftImg, dftTempl1, dftImg, 0, true);
+            dft( dftImg, dftImg, DFT_INVERSE + DFT_SCALE, bsz.height );
+
+            src = dftImg(Rect(0, 0, bsz.width, bsz.height));
+
+            if( ccn > 1 )
+            {
+                if( cdepth != maxDepth )
+                {
+                    Mat plane(bsz, cdepth, &buf[0]);
+                    src.convertTo(plane, cdepth, 1, delta);
+                    src = plane;
+                }
+                int pairs[] = {0, k};
+                mixChannels(&src, 1, &cdst, 1, pairs, 1);
+            }
+            else
+            {
+                if( k == 0 )
+                    src.convertTo(cdst, cdepth, 1, delta);
+                else
+                {
+                    if( maxDepth != cdepth )
+                    {
+                        Mat plane(bsz, cdepth, &buf[0]);
+                        src.convertTo(plane, cdepth);
+                        src = plane;
+                    }
+                    add(src, cdst, cdst);
+                }
+            }
+        }
+    }
 }
 
-cv::Mat L(const cv::Mat &I)
-{
-  cv::Mat L_I, L_F;
-  cv::cvtColor(I, L_I, CV_BGR2GRAY);
-  L_I.convertTo(L_F, CV_64F);
-  return L_F;
-}
 
 //Correlation
 
@@ -747,15 +806,6 @@ void correlationFFTInBatch(Mat& img, vector<MAXLOCAL> &out, vector<KERNEL> kers,
 	KERNEL tmp;
 	Mat_<FLT> corrMat;
 
-	Mat tF;
-	Mat inF; 
-	convertGRYToFLT(img, inF);
-	inF.convertTo(inF, CV_64F);
-
-	cv::Size size = fit(inF.size());
-
-	fF_I = F_fwd(inF, size);
-
 	std::vector<MAXLOCAL> v;
 
 	for(unsigned int i = 0; i < kers.size(); i++){
@@ -763,9 +813,11 @@ void correlationFFTInBatch(Mat& img, vector<MAXLOCAL> &out, vector<KERNEL> kers,
 
 		printf("Applying filter #%u-#%lu\r", i+1, kers.size());
 		fflush(stdout); 
-		
-		tmp.imgF.convertTo(tF, CV_64F);
-		corrMat = FC(inF, tF, size);
+
+		Size corrSize(img.cols - tmp.imgF.cols + 1, img.rows - tmp.imgF.rows + 1);
+		corrMat.create(corrSize);
+
+		crossCorr( img, tmp.imgF, corrMat, corrMat.size(), corrMat.type(), Point(0,0), 0, 0);
 
 		for (int l=1; l<corrMat.rows-1; l++){
 			for (int c=1; c<corrMat.cols-1; c++) {
